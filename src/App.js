bb4eaa578e2b6480be3cf3de8809b8377025e1bb
vanodee <vanodee007@gmail.com>
2024-01-31 20:57:16 +0100

    Header, Footer & Meta Tags

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
 import Main from './Components/Main';
@@ -5,11 +6,22 @@ import Footer from './Components/Footer';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
